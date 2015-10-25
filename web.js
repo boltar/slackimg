eb9c5f7c1810eb6ad57f49bf066f0c869d1ce08a
Tony Cha <tony.cha@gmail.com>
2015-10-24 21:49:25 -0400

    fix typo, add stingtalk

--- web.js
+++ web.js
@@ -128,26 +128,24 @@ function PostToSlack(channel_name, post_text, bot_name, bot_emoji) {
 	//'", "icon_emoji" : "' + bot_emoji + '"}';
   
   console.log(post_text)
-  var post_options = {
-      host: 'poundc.slack.com',
-      port: '443',
-      path: path_str,
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': Buffer.byteLength(post_data, 'utf8')
-      }
-  };
 
   if (channel_name === "legible")
   {
     //#legible
     path_str = '/services/hooks/incoming-webhook?token=mcmbhcqQpfoU2THsofvad3VA'; 
-  } else if (channel_name == football) {
+    host_str = 'poundc.slack.com';
+  } else if (channel_name == "football") {
     //#football, stingtalk
     path_str = 'https://hooks.slack.com/services/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';
-      post_options = {
-      host: 'stingtalk.slack.com',
+    host_str = 'stingtalk.slack.com'
+  } else if (channel_name == "testing") {
+    //#testing
+    path_str = 'https://hooks.slack.com/services/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'; 
+    host_str = 'poundc.slack.com'    
+  }
+
+  var post_options = {
+      host: host_str,
       port: '443',
       path: path_str,
       method: 'POST',
@@ -156,10 +154,6 @@ function PostToSlack(channel_name, post_text, bot_name, bot_emoji) {
           'Content-Length': Buffer.byteLength(post_data, 'utf8')
       }
   };
-  } else if (channel_name == testing) {
-    //#testing
-    path_str = 'https://hooks.slack.com/services/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'; 
-  }
 
   // Set up the request
   var post_req = https.request(post_options, function(res) {
